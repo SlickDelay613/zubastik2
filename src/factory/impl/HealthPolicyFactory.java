d5ea972cd6872f8e5b82a3fca72851afceffb8e8
SlickDelay614 <234567gleb@gmail.com>
2026-03-28 09:55:44 +0300

    Supersus

--- src/factory/impl/HealthPolicyFactory.java
+++ src/factory/impl/HealthPolicyFactory.java
@@ -6,15 +6,17 @@ import model.CustomerDto;
 import model.PolicyDto;
 import helper.Numerator;
 
+import static helper.PolicyType.HEALTH;
+
 public class HealthPolicyFactory implements PolicyFactory {
     @Override
     public PolicyDto createPolicy(CustomerDto customer, double coverageAmount, double premium) {
         String policyNumber = String.valueOf(Numerator.makeNewNumberForType(PolicyDto.class));
-        return new PolicyDto(policyNumber, customer, coverageAmount, premium, PolicyType.HEALTH);
+        return new PolicyDto(policyNumber, customer, coverageAmount, premium, HEALTH);
     }
 
     @Override
     public PolicyType getSupportedType() {
-        return PolicyType.HEALTH;
+        return HEALTH;
     }
 }
--- src/factory/impl/PropertyPolicyFactory.java
+++ src/factory/impl/PropertyPolicyFactory.java
@@ -6,15 +6,17 @@ import model.CustomerDto;
 import model.PolicyDto;
 import helper.Numerator;
 
+import static helper.PolicyType.PROPERTY;
+
 public class PropertyPolicyFactory implements PolicyFactory {
     @Override
     public PolicyDto createPolicy(CustomerDto customer, double coverageAmount, double premium) {
         String policyNumber = String.valueOf(Numerator.makeNewNumberForType(PolicyDto.class));
-        return new PolicyDto(policyNumber, customer, coverageAmount, premium, PolicyType.PROPERTY);
+        return new PolicyDto(policyNumber, customer, coverageAmount, premium, PROPERTY);
     }
 
     @Override
     public PolicyType getSupportedType() {
-        return PolicyType.PROPERTY;
+        return PROPERTY;
     }
 }
